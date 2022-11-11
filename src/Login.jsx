7c338efc29bf3af9071c837c4727f6ca92a46995
naveenb13 <naveenb13598@gmail.com>
2022-11-12 01:30:57 +0530

    resolution logsign

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,6 +13,24 @@ function Login() {
       email: "",
       password: "",
     },
+    validate : (values) => {
+
+      let errors = {}
+
+      if(values.email === ""){
+          errors.email = "Please enter email"
+      }
+
+      if(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{3}$/i.test(values.email) !== true){
+          errors.email = "Please enter a Valid email ID"
+      }
+
+      if(values.password === ""){
+          errors.password = "Please enter Password"
+      }
+
+      return errors;
+  },
     onSubmit: async (values) => {
       try {
         let loginData = await axios.post(`${env.api}/login`, values)
@@ -39,12 +57,12 @@ function Login() {
                 <div className="loginresp1">
                   <label for="exampleInputEmail1" className="form-label mb-3">Email address</label>
                   <input name='email' type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={formik.values.email} onChange={formik.handleChange} />
-                  <div id="emailHelp" className="form-text text-danger">We'll never share your email with anyone else.</div>
+                  <span className='validation'>{formik.errors.email}</span>
                 </div>
                 <div className="loginresp1">
                   <label for="exampleInputPassword1" className="form-label mb-3">Password</label>
                   <input type="password" className="form-control" id="exampleInputPassword1" name="password" value={formik.values.password} onChange={formik.handleChange} />
-                  <div id="emailHelp" className="form-text loginresp text-danger">We'll never share your email with anyone else.</div>
+                  <span className='validation'>{formik.errors.password}</span>
                 </div>
                 <div className="d-grid gap-2">
                   <button type={"submit"} className="btn btn-success loginresp1">Login</button>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -15,8 +15,28 @@ function Signup() {
       email: "",
       password: "",
     },
-    validate: (values) => {
-    },
+    validate : (values) => {
+
+      let errors = {}
+
+      if(values.username.length <= 5){
+        errors.username = "Username must be greater than 5 letters"
+    }
+
+      if(values.email === ""){
+          errors.email = "Please enter email"
+      }
+
+      if(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{3}$/i.test(values.email) !== true){
+          errors.email = "Please enter a Valid email ID"
+      }
+
+      if(values.password.length <= 7){
+          errors.password = "Password must be greater than 7 letters"
+      }
+
+      return errors;
+  },
     onSubmit: async (values) => {
       let signup = await axios.post(`${env.api}/register`, values)
       console.log(signup)
@@ -27,28 +47,28 @@ function Signup() {
 
   return (
     <div className='loginheading pt-3'><h1 className='btn4'>OUT<span className='btn3'>colors</span>FIT</h1>
-      <div className='loginpage'>
-        <div className='container1'>
+      <div className='signuppage'>
+        <div className='container2'>
           <div className='row'>
             <div className='col-7'>
-              <h1 className='my-5'><b>SIGNUP / REGISTRATION</b></h1>
+              <h1 className='signhead'><b>SIGNUP / REGISTRATION</b></h1>
               <form className='loginprop' onSubmit={formik.handleSubmit}>
-                <div className="mb-3">
-                  <label for="exampleInputEmail1" className="form-label mb-2">First name</label>
+                <div className="signmargin">
+                  <label for="exampleInputEmail1" className="form-label mb-2 label1">First name</label>
                   <input type={"text"} aria-label="First name" class="form-control" name='username' value={formik.values.username} onChange={formik.handleChange} />
-                  <div id="emailHelp" className="form-text text-danger">We'll never share your email with anyone else.</div>
+                  <span className='validation'>{formik.errors.username}</span>
                 </div>
-                <div className="mb-3">
-                  <label for="exampleInputEmail1" className="form-label mb-2">Email address</label>
+                <div className="signmargin">
+                  <label for="exampleInputEmail1" className="form-label mb-2 label1">Email address</label>
                   <input type={"email"} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={formik.values.email} onChange={formik.handleChange} />
-                  <div id="emailHelp" className="form-text text-danger">We'll never share your email with anyone else.</div>
+                  <span className='validation'>{formik.errors.email}</span>
                 </div>
-                <div className="mb-3">
-                  <label for="exampleInputPassword1" className="form-label mb-2">Password</label>
+                <div className="signmargin">
+                  <label for="exampleInputPassword1" className="form-label mb-2 label1">Password</label>
                   <input type={"password"} className="form-control" id="exampleInputPassword1" name='password' value={formik.values.password} onChange={formik.handleChange} />
-                  <div id="emailHelp" className="form-text mb-5 text-danger">We'll never share your email with anyone else.</div>
+                  <span className='validation'>{formik.errors.password}</span>
                 </div>
-                <div className="d-grid gap-2">
+                <div className="d-grid gap-2 py-4">
                   <button type={"submit"} className="btn btn-success" >Signup</button>
                 </div>
               </form>
